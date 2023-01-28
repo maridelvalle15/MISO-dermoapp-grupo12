e857f94baf29f33988321d4ac4d544e1d8b4d645
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-01-27 19:21:31 -0500

    probando base de datos de prueba en ci

--- autenticacion/tests/conftest.py
+++ autenticacion/tests/conftest.py
@@ -1,7 +1,11 @@
 from flaskr import app
 import pytest
 
+
 @pytest.fixture
 def client():
+    app.app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    #app.app.config['TESTING'] = True
+    
     with app.app.test_client() as client:
         yield client
