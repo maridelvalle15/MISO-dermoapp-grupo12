dbc83c1d2244fb274960bcd4c5a6e5104bee3116
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-01-27 23:01:12 -0500

    sqlite para pruebas

--- autenticacion/tests/conftest.py
+++ autenticacion/tests/conftest.py
@@ -4,8 +4,6 @@ import pytest
 
 @pytest.fixture
 def client():
-    app.app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-    #app.app.config['TESTING'] = True
-    
+        
     with app.app.test_client() as client:
         yield client
