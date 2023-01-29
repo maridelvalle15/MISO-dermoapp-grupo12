a54083d27d164c7cf1b84ff88ee97815de20febf
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-01-28 22:39:47 -0500

    agregadas pruebas para registro medico

--- autenticacion/tests/test_registro.py
+++ autenticacion/tests/test_registro.py
@@ -1,4 +1,5 @@
 import json
+from flaskr.models import Ubicacion, Especialidad
 
 class TestRegistro:
     def test_crear_usuario_existente(self,client,headers,crear_usuario_medico):
@@ -6,8 +7,6 @@ class TestRegistro:
         usuario = crear_usuario_medico
 
         data = {
-            'password1': 'password1',
-            'password2': 'password1',
             'email': usuario.email
         }
 
@@ -15,3 +14,66 @@ class TestRegistro:
 
         assert response.status_code==400
         assert response.json['message'] == 'El usuario ya existe'
+
+    def test_ubicacion_invalida(self,client,headers,crear_usuario_medico):
+
+        usuario = crear_usuario_medico
+
+        data = {
+            'tipo_usuario': 'MEDICO',
+            'email': usuario.email + 'nuevoEmail',
+            'nombre': 'nombre prueba',
+            'direccion': 'direccion prueba',
+            'pais': 'pais no valido',
+            'ciudad': 'ciudad no valida',
+        }
+
+        response = client.post('/api/registro', data=json.dumps(data), headers=headers)
+
+        assert response.status_code==400
+        assert response.json['message'] == 'ubicacion no valida'
+
+    def test_especialidad_invalida(self,client,headers,crear_usuario_medico):
+
+        usuario = crear_usuario_medico
+
+        ubicacion = Ubicacion.query.first()
+
+        data = {
+            'tipo_usuario': 'MEDICO',
+            'email': usuario.email + 'nuevoEmail',
+            'nombre': 'nombre prueba',
+            'direccion': 'direccion prueba',
+            'pais': ubicacion.pais,
+            'ciudad': ubicacion.ciudad,
+            'especialidad': 'especialidad no valida'
+        }
+
+        response = client.post('/api/registro', data=json.dumps(data), headers=headers)
+
+        assert response.status_code==400
+        assert response.json['message'] == 'especialidad no valida'
+
+    def test_crear_usuario_exitosamente(self,client,headers,crear_usuario_medico):
+        usuario = crear_usuario_medico
+
+        ubicacion = Ubicacion.query.first()
+        especialidad = Especialidad.query.first()
+
+        data = {
+            'tipo_usuario': 'MEDICO',
+            'email': usuario.email + 'nuevoEmail',
+            'nombre': 'nombre prueba',
+            'direccion': 'direccion prueba',
+            'pais': ubicacion.pais,
+            'ciudad': ubicacion.ciudad,
+            'especialidad': especialidad.nombre,
+            'licencia': 'XX12900s'
+        }
+
+        response = client.post('/api/registro', data=json.dumps(data), headers=headers)
+
+        assert response.status_code==200
+        assert response.json['message'] == 'usuario creado exitosamente'
+        assert response.json['password'] != ''
+        assert response.json['password'] is not None
