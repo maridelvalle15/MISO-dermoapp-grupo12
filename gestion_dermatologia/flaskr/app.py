d5d5dccb76d401b0c09da96ea6656dd03c46f8bc
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-02-02 15:50:05 -0500

    llamado exitoso al ms de autenticacion

--- gestion_dermatologia/flaskr/app.py
+++ gestion_dermatologia/flaskr/app.py
@@ -2,16 +2,19 @@ from flaskr import create_app
 from flask_restful import Api
 from .models import db
 from .views import SuministroLesionView
+import logging
 
-app = create_app('default')
+
+app = create_app('gestion_dermatologia')
 app_context = app.app_context()
 app_context.push()
+logging.basicConfig(level=logging.DEBUG)
+
 
 db.init_app(app)
 db.create_all()
 
 api = Api(app)
-
 
 api.add_resource(SuministroLesionView, '/api/suministro-lesion')
 
--- gestion_dermatologia/flaskr/views/views.py
+++ gestion_dermatologia/flaskr/views/views.py
@@ -1,10 +1,18 @@
 from flask_restful import Resource
-from flask import request
-import requests
+from flask import request, jsonify
+import requests, os, json
 
 
 class SuministroLesionView(Resource):
 
     def post(self):
-
-        return {"message":"hola"}, 200
+        auth_url_validacion_usuario = os.environ.get("AUTH_BASE_URI") + '/api/validacion-usuario'
+        headers = {'Authorization': request.headers.get('Authorization')}
+    
+        response = requests.get(auth_url_validacion_usuario, headers=headers)
+        #print(json.loads(response.content.decode('utf8').replace("'", '"')), flush=True)
+        json_response=json.loads(response.content.decode('utf8').replace("'", '"'))
+        if response.status_code == 200:
+            return {"message":"hola","rol": json_response['rol']}, 200
+        else:
+            return {"message":"Unauthorized"}, 401
