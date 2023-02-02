a0d73832012ad4bc236c8a56cbc2561183f7a140
Oscar Ortiz <oscarm.ortizf@gmail.com>
2023-02-02 14:14:30 -0500

    Added basic Case listing feature

---
+++ app/lib/common/functions/CaseListManager.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dermoapp/common/functions/getToken.dart';
+import 'package:dermoapp/model/caseModel.dart';
+import 'package:http/http.dart' as http;
+
+final http.Client client = http.Client();
+
+Future<List> getMyCases() async {
+  String token = await getToken() as String;
+  final response = await client.get(
+    Uri.parse('https://c6f0-186-80-52-161.ngrok.io/api/caso'),
+    headers: {
+      HttpHeaders.authorizationHeader: 'Basic $token',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    var responseJson = json.decode(response.body);
+
+    List<dynamic> cases = [];
+    for (final caseResponseTag in responseJson) {
+      if (caseResponseTag["cases"] != null) {
+        cases = caseResponseTag["cases"]
+            .map<CaseModel>((json) => CaseModel.fromJson(json))
+            .toList();
+        break;
+      }
+    }
+
+    return cases;
+  } else {
+    return List.empty();
+  }
+}
---
+++ app/lib/ui/caseListScreen.dart
@@ -0,0 +1,139 @@
+import 'package:dermoapp/common/functions/CaseListManager.dart';
+import 'package:dermoapp/common/widgets/mainDrawer.dart';
+import 'package:dermoapp/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:country_icons/country_icons.dart';
+
+class CaseListScreen extends StatefulWidget {
+  const CaseListScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return CaseListScreenState();
+  }
+}
+
+class CaseListScreenState extends State<CaseListScreen> {
+  List results = [];
+  /*@override
+  void initState() {
+    super.initState();
+    casesList = getMyCases();
+    print(casesList);
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: const MainDrawer(
+          currentSelected: 3,
+        ),
+        appBar: AppBar(
+            title: Text(AppLocalizations.of(context).myCases,
+                style: const TextStyle(fontSize: 14)),
+            actions: <Widget>[
+              IconButton(
+                icon: Image.asset('icons/flags/png/es.png',
+                    package: 'country_icons'),
+                onPressed: () => DermoApp.of(context)!
+                    .setLocale(const Locale.fromSubtags(languageCode: 'es')),
+              ),
+              IconButton(
+                icon: Image.asset('icons/flags/png/us.png',
+                    package: 'country_icons'),
+                onPressed: () => DermoApp.of(context)!
+                    .setLocale(const Locale.fromSubtags(languageCode: 'en')),
+              ),
+            ]),
+        body: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.topCenter,
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                  child: Text("Dermoapp",
+                      style:
+                          TextStyle(fontSize: 40.0, color: Color(0xffdfdfdf))),
+                )),
+            Container(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+                  child: Text(AppLocalizations.of(context).myCases,
+                      style: const TextStyle(
+                          fontSize: 16.0, color: Color(0xffdfdfdf))),
+                )),
+            SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.all(10),
+                child: FutureBuilder(
+                    future: getMyCases(),
+                    builder: (context, snapshot) {
+                      results = snapshot.data ?? [];
+                      return FittedBox(
+                        child: DataTable(
+                            headingTextStyle: const TextStyle(
+                                color: Color(0xFFDFDFDF),
+                                fontWeight: FontWeight.bold),
+                            dataTextStyle:
+                                const TextStyle(color: Color(0xFFDFDFDF)),
+                            columns: <DataColumn>[
+                              DataColumn(
+                                label: Text(
+                                  AppLocalizations.of(context).id,
+                                ),
+                              ),
+                              DataColumn(
+                                label: Text(
+                                  AppLocalizations.of(context).caseDate,
+                                ),
+                              ),
+                              DataColumn(
+                                label: Text(AppLocalizations.of(context)
+                                    .diagnosticType),
+                              ),
+                              DataColumn(
+                                label: Text(
+                                    AppLocalizations.of(context).caseDetails),
+                              ),
+                            ],
+                            rows: List.generate(results.length,
+                                (index) => getDataRow(index, results[index]))),
+                      );
+                    })),
+          ],
+        )));
+  }
+
+  DataRow getDataRow(index, data) {
+    int caseId = data.id;
+    String diagnosticType = data.diagnosticType == 'auto'
+        ? AppLocalizations.of(context).automatic
+        : AppLocalizations.of(context).manual;
+    return DataRow(
+      cells: <DataCell>[
+        DataCell(
+          Text('$caseId'),
+        ),
+        DataCell(
+          Text(data.date),
+        ),
+        DataCell(
+          Text(diagnosticType),
+        ),
+        DataCell(
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(AppLocalizations.of(context).caseDetails,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.normal)),
+          ),
+        ),
+      ],
+    );
+  }
+}
