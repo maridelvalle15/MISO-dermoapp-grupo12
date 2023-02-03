326512f6303e7c082f811f5bc601c56e4e38375c
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-02-03 15:35:04 -0500

    reestructurando proyecto: no se usara ms gestion_casos

--- gestion_casos/flaskr/models/models.py
+++
@@ -1,4 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-
-db = SQLAlchemy()
--- gestion_casos/flaskr/views/views.py
+++
@@ -1,5 +0,0 @@
-from flask_restful import Resource
-
-class HolaView(Resource):
-    def get(self):
-        return "hola"
