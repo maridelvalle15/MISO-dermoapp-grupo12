0cfcb3cd233b18425fccd61c864aa10c6912361e
JuanOliveros <j.oliverosv@uniandes.edu.co>
2023-02-05 17:49:48 -0500

    LISTAR CASOS
    listar casos

--- front/src/app/caso/caso-listar/caso-listar.component.ts
+++ front/src/app/caso/caso-listar/caso-listar.component.ts
@@ -15,7 +15,7 @@ interface Student{
 })
 export class CasoListarComponent implements OnInit {
 
-  casos: Caso[] =[];
+  casos: any;
   constructor(private casoService: CasoService,) {}
 
 
@@ -28,7 +28,7 @@ export class CasoListarComponent implements OnInit {
     this.casoService.getCasos().subscribe(casos => {
       this.casos = casos;
       console.log(casos)
-      this.casos = Object.values(this.casos)
+      this.casos = Object.values(this.casos.casos)
     })
 
   }
