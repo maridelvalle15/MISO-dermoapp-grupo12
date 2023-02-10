d7d3c8499d9e57c05f6d2f137fff290bcf087e48
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-02-09 21:23:32 -0500

    agregadas pruebas de validacion de acceso a servicios entre roles

--- servicios_salud/tests/test_suministro_lesion.py
+++ servicios_salud/tests/test_suministro_lesion.py
@@ -2,7 +2,7 @@ from flaskr.models import *
 import pytest, flask
 
 class TestRegistro:
-    def test_medico_no_puede_llamar_servicio(self,client,mocker):
+    def test_medico_no_puede_llamar_servicio_crear_caso(self,client,mocker):
         
         request_mock = mocker.patch("requests.get")
         request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Medico", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
@@ -37,3 +37,29 @@ class TestRegistro:
 
         assert response.status_code==200
         assert response.json['message'] == 'Caso creado exitosamente'
+
+    def test_medico_no_puede_llamar_servicio_casos_de_un_paciente(self,client,mocker):
+        
+        request_mock = mocker.patch("requests.get")
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Medico", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
+
+        response = client.get('/api/suministro-lesion')
+
+        assert response.status_code==401
+        assert response.json['message'] == 'Unauthorized'
+
+    def test_paciente_no_puede_llamar_servicio_casos_pacientes(self,client,mocker):
+        
+        request_mock = mocker.patch("requests.get")
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Paciente", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
+
+        response = client.get('/api/casos-pacientes')
+
+        assert response.status_code==401
+        assert response.json['message'] == 'Unauthorized'
