aa1d03a585608a0c8884154d38997c5abdcb3104
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-02-09 21:17:21 -0500

    ajuste en pruebas

--- servicios_salud/tests/test_suministro_lesion.py
+++ servicios_salud/tests/test_suministro_lesion.py
@@ -5,7 +5,10 @@ class TestRegistro:
     def test_medico_no_puede_llamar_servicio(self,client,mocker):
         
         request_mock = mocker.patch("requests.get")
-        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Medico"}\n'
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Medico", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
 
         response = client.post('/api/suministro-lesion')
 
@@ -15,8 +18,11 @@ class TestRegistro:
     def test_paciente_crea_caso(self,client,mocker):
         
         request_mock = mocker.patch("requests.get")
-        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Paciente", "tipo_piel": "normal"}\n'
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Paciente", "tipo_piel": "normal", "especialidad": "", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
         request_mock.return_value.status_code = 200
+        
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
 
         data = {
             'tipo': 'mac',
