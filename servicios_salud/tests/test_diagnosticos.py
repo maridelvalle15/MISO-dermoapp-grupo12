49397de5b22dc687586a39a25816cacde0c42416
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-02-15 21:16:07 -0500

    test diagnostico automatico

--- servicios_salud/tests/test_diagnosticos.py
+++ servicios_salud/tests/test_diagnosticos.py
@@ -11,3 +11,15 @@ class TestDiagnosticos():
 
         assert response.status_code==200
         assert "diagnostico" in response.json
+
+    def test_diagnostico_solo_se_genera_una_vez(self,client,headers,crear_caso):
+        caso = crear_caso
+        data = {
+            'caso_id': caso.id,
+        }
+
+        response = client.post('/api/diagnostico-automatico',data=json.dumps(data),headers=headers)
+        response2 = client.post('/api/diagnostico-automatico',data=json.dumps(data),headers=headers)
+
+        assert response2.status_code==400
+        assert response2.json['message'] == 'No es posible realizar un diagnóstico al caso asignado'
