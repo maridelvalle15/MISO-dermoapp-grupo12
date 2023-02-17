233d9e1d5e2225f9281488f3dfd32f4572ec8239
JuanOliveros <j.oliverosv@uniandes.edu.co>
2023-02-17 13:46:59 -0500

    Ajuste documento pruebas

--- front/src/app/diagnostico/diagnostico-listar/diagnostico-listar.component.spec.ts
+++ front/src/app/diagnostico/diagnostico-listar/diagnostico-listar.component.spec.ts
@@ -5,12 +5,31 @@ import { DebugElement } from '@angular/core';
 
 import { DiagnosticoListarComponent } from './diagnostico-listar.component';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppHeaderModule } from 'app/app-header/app-header.module';
+import { faker } from '@faker-js/faker';
+import { MaterialModule } from 'app/material/material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('DiagnosticoListarComponent', () => {
   let component: DiagnosticoListarComponent;
   let fixture: ComponentFixture<DiagnosticoListarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+        AppHeaderModule,
+        MaterialModule,
+        BrowserAnimationsModule
+      ],
       declarations: [ DiagnosticoListarComponent ]
     })
     .compileComponents();
