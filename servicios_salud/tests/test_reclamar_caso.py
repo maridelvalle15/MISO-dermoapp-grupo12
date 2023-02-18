9eb7dc91c47850d84a3d4d4b54b5bc0e13c6d89d
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-02-17 22:42:16 -0500

    pruebas para medico obtiene casos reclamados

--- servicios_salud/tests/test_reclamar_caso.py
+++ servicios_salud/tests/test_reclamar_caso.py
@@ -39,3 +39,30 @@ class TestReclamarCaso:
         assert response.status_code==200
         assert response.json['message'] == 'Caso asignado'
         assert response.json['caso_id'] == caso.id
+
+    def test_paciente_no_puede_obtener_casos_reclamados(self,client,mocker):
+        
+        request_mock = mocker.patch("requests.get")
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Paciente", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
+
+        response = client.get('/api/reclamar-caso')
+
+        assert response.status_code==401
+        assert response.json['message'] == 'Unauthorized'
+
+    def test_medico_obtiene_casos_reclamados(self,client,mocker):
+        
+        request_mock = mocker.patch("requests.get")
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Medico", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
+        request_mock.return_value.status_code = 200
+
+        response = client.get('/api/reclamar-caso')
+
+        assert response.status_code==200
+        assert "casos" in response.json
