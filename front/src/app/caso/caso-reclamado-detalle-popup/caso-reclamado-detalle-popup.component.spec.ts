55ec68b51b080a6b6142348100877ea98d4c80a6
JuanOliveros <j.oliverosv@uniandes.edu.co>
2023-03-07 21:51:53 -0500

    Update caso-reclamado-detalle-popup.component.spec.ts

--- front/src/app/caso/caso-reclamado-detalle-popup/caso-reclamado-detalle-popup.component.spec.ts
+++ front/src/app/caso/caso-reclamado-detalle-popup/caso-reclamado-detalle-popup.component.spec.ts
@@ -3,26 +3,81 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppHeaderModule } from 'app/app-header/app-header.module';
+import { MaterialModule } from 'app/material/material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { faker } from '@faker-js/faker';
+import { Caso } from '../caso';
+import { CasoService } from '../caso.service';
+
 import { CasoReclamadoDetallePopupComponent } from './caso-reclamado-detalle-popup.component';
 
 describe('CasoReclamadoDetallePopupComponent', () => {
   let component: CasoReclamadoDetallePopupComponent;
   let fixture: ComponentFixture<CasoReclamadoDetallePopupComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CasoReclamadoDetallePopupComponent ]
-    })
-    .compileComponents();
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+        AppHeaderModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        MatDialogModule
+      ],
+      declarations: [
+        CasoReclamadoDetallePopupComponent
+      ],
+      providers : [
+        CasoService
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CasoReclamadoDetallePopupComponent);
     component = fixture.componentInstance;
+
+    let testCasos : Array<Caso>=[];
+
+    for (let i=0; i<2; i++){
+      testCasos[i] = new Caso(
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+
+      )
+    }
+    component.caso = testCasos;
     fixture.detectChanges();
+    debug = fixture.debugElement;
+
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const fixture = TestBed.createComponent(CasoReclamadoDetallePopupComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
+
+
