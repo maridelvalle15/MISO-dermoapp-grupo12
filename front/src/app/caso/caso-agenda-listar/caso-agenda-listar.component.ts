efe2f16bd5c7c1270938bf8a8207ac403c02ce00
JuanOliveros <j.oliverosv@uniandes.edu.co>
2023-03-11 20:00:38 -0500

    Avances

---
+++ front/src/app/caso/caso-agenda-listar/caso-agenda-listar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CasoService } from '../caso.service';
+
+@Component({
+  selector: 'app-caso-agenda-listar',
+  templateUrl: './caso-agenda-listar.component.html',
+  styleUrls: ['./caso-agenda-listar.component.css']
+})
+export class CasoAgendaListarComponent implements OnInit {
+  citas: any;
+  constructor(
+    private casoService: CasoService
+    ) { }
+
+  ngOnInit() {
+   // this.getAgenda();
+  }
+
+  getAgenda(){
+    this.casoService.getAgenda().subscribe(citas => {
+      this.citas = citas;
+      //this.citas = Object.values(this.citas)
+    })
+
+
+  }
+
+}
