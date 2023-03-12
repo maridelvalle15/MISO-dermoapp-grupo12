970b1c132c4dd0334201b40164b8bcc28e5fdf5b
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2023-03-11 23:52:32 -0500

    pruebas para obtener agenda de medico

---
+++ servicios_salud/tests/test_agenda_medico.py
@@ -0,0 +1,29 @@
+import json, pytest
+
+class TestAgendaMedico():
+    def test_paciente_no_ve_agenda_medico(self,client,mocker):
+        request_mock = mocker.patch("requests.get")
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Paciente", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion"}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
+        request_mock.return_value.status_code = 401
+
+        response = client.get('/api/agenda-medico')
+
+        assert response.status_code==401
+        assert response.json['message'] == 'Unauthorized'
+
+    @pytest.mark.parametrize('crear_cita', [['3']], indirect=True)
+    def test_medico_ve_su_agenda(self,client,mocker,headers,crear_cita):
+        request_mock = mocker.patch("requests.get")
+        request_mock.return_value.content = b'{"id_usuario": 3, "rol": "Medico", "especialidad": "especialidad", "tipo_piel":"", "nombre":"nombre", "ubicacion_id": "ubicacion", "id_medico": 3}\n'
+
+        os_mock = mocker.patch("os.environ.get")
+        os_mock.return_value.content = ''
+        request_mock.return_value.status_code = 200
+
+        response = client.get('/api/agenda-medico', headers=headers)
+
+        assert response.status_code==200
+        assert "agenda" in response.json
